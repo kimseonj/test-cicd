c23e88c9654395f50a0835e58c728b512b564355
Seonjun Kim <kimsunjun1221@gmail.com>
2025-11-25 14:29:57 +0900

    blue-green을 사용한 zero-downtime deployment

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/health', (req, res) => {
+	const now = new Date();
+	const hh = String(now.getHours()).padStart(2, '0');
+	const mm = String(now.getMinutes()).padStart(2, '0');
+	const ss = String(now.getSeconds()).padStart(2, '0');
+	const timeString = `${hh}:${mm}:${ss}`;
+
+	res.status(200).send(`OK - ${timeString} \n`);
+});
+
 app.get('/', (req, res) => {
 	res.send('Hello, World!');
 });
